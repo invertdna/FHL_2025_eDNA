8a536cfd6a22c408c0f1d02a7cdce93870f91f4c
Gled <51749738+gledguri@users.noreply.github.com>
2025-10-06 17:06:37 -0700

    update

--- code/GG_betareg.R
+++ code/GG_betareg.R
@@ -1,6 +1,6 @@
 # Libraries -----------------------------------------------------------------------------------
+library(betareg)
 library(vegan)
-library(rstan);options(mc.cores = parallel::detectCores())
 library(ggplot2)
 library(dplyr)
 library(here)
@@ -16,13 +16,13 @@ make_index <- function(data, index_variable, index_name) {
 		ungroup()
 }
 
-extract_param <- function (model, par) 
-{
-	fit <- (methods::selectMethod("summary", signature = "stanfit"))(object = model, 
-																																	 par = par)
-	fit <- fit$summary
-	return(fit %>% unlist() %>% as.data.frame %>% round(., 9))
-}
+# extract_param <- function (model, par) 
+# {
+# 	fit <- (methods::selectMethod("summary", signature = "stanfit"))(object = model, 
+# 																																	 par = par)
+# 	fit <- fit$summary
+# 	return(fit %>% unlist() %>% as.data.frame %>% round(., 9))
+# }
 
 
 logit <- function(p) {
@@ -79,7 +79,10 @@ design_gg_mat <- function(dist_matrix,metadata){
 		mutate(delta_date=date_2-date_1) %>% 
 		mutate(rep_cat=if_else(time_cat==0&tre_cat==0&delta_date==0,1,0)) %>% 
 		filter(!(time_cat==1&tre_cat==1)) %>% #remove the combinations when Time of Day changes (Day v Night) and Treatment changes (Light v No light)
-		mutate(beta_idx=ifelse(time_1=='night',2,1))
+		mutate(beta_idx=ifelse(time_1=='night',2,1)) %>% 
+		mutate(beta_idx=if_else(tre_cat==0,0,beta_idx)) %>% 
+		mutate(light_day=if_else(beta_idx==1,1,0),
+					 light_night=if_else(beta_idx==2,1,0)) %>% select(-beta_idx)
 	# filter(!delta_date==7) 
 	# select(Tube_1,Tube_2,D,time_cat,tre_cat,delta_date,rep_cat,beta_idx)
 	
@@ -122,6 +125,7 @@ dist_mv1_prop <- mv1_prop %>% t() %>% vegdist(method = "bray", binary = T)
 dist_mv1_prop_comm <- mv1_prop_comm %>% t() %>% vegdist(method = "bray", binary = T)
 
 
+# Create design matrix ------------------------------------------------------------------------
 design_mat_coi_eDNAidx <- design_gg_mat(dist_matrix = dist_coi_eDNAidx,metadata = coi_meta)
 design_mat_coi_bin <- design_gg_mat(dist_matrix = dist_coi_bin,metadata = coi_meta)
 design_mat_coi_prop <- design_gg_mat(dist_matrix = dist_coi_prop,metadata = coi_meta)
@@ -143,6 +147,7 @@ nrow(design_mat_coi_eDNAidx) #The amount of pairwise comparisons retained and fa
 #The amount of pairwise comparisons removed
 estimate_length_of_pairs(ncol(coi_eDNAidx))-nrow(design_mat_coi_eDNAidx)
 
+#Create a list for all the design matricies so that it is easier to do for-loops downstream
 design_mat_list <- list(design_mat_coi_eDNAidx,
 												design_mat_coi_bin,
 												design_mat_coi_prop,
@@ -154,8 +159,9 @@ design_mat_list <- list(design_mat_coi_eDNAidx,
 												design_mat_mv1_eDNAidx,
 												design_mat_mv1_bin,
 												design_mat_mv1_prop #design_mat_mv1_prop_comm
-												)
+)
 
+#Create a list for all the names of the design matricies so that it is easier to know which matrix belongs to which in the for-loops downstream
 names_of_iterations <- c('coi_eDNAidx',
 												 'coi_presence_absence',
 												 'coi_prop',
@@ -167,132 +173,134 @@ names_of_iterations <- c('coi_eDNAidx',
 												 'mv1_eDNAidx',
 												 'mv1_presence_absence',
 												 'mv1_relative_abundance' #'mv1_relative_abundance_common_sp'
-												 )
+)
 
+# This is an example (here is for COI and eDNA index) of a design matrix
 design_mat_coi_eDNAidx %>% 
-	mutate(beta_idx=if_else(tre_cat==0,0,beta_idx)) %>% 
-	count(time_cat,tre_cat,delta_date,rep_cat,beta_idx) %>% 
+	count(time_cat,tre_cat,light_day,light_night,delta_date,rep_cat) %>% 
 	rename(Day_v_night='time_cat') %>% 
 	rename(Date_difference='delta_date') %>% 
 	rename(Light_v_Nolight='tre_cat') %>% 
-	rename(Biological_replciate='rep_cat') %>% 
-	rename(Day1_Night_2='beta_idx')
+	rename(Biological_replciate='rep_cat')
+
+# Create a list for capturing all the model outputs
+s <- vector('list')
 
 for (k in 1:length(names_of_iterations)) {
-design_mat <- design_mat_list[[k]]
-
-stan_data <- list(
-	N = nrow(design_mat),         
-	# D = design_mat$D_comm,
-	D = design_mat$D,
-	delta_date = design_mat$delta_date,
-	time_cat = design_mat$time_cat,
-	tre_cat = design_mat$tre_cat,
-	rep_cat = design_mat$rep_cat,
-	beta_idx = design_mat$beta_idx,
-	J = max(design_mat$beta_idx)
-)
-
-
-fit <- stan(
-	file = "/Users/gledguri/Local/GitHub/FHL_2025_eDNA/code/model.stan",
-	data = stan_data,
-	chains = 4,
-	iter = 2000,
-	# init = init_list,
-	warmup = 1000)
-
-
-output <- extract_param(model = fit,par = c('gamma','alpha','beta','theta','intercept','phi')) %>% 
-	rownames_to_column('param') %>% 
-	mutate(param=if_else(param=='beta[1]','Light_day',param)) %>% 
-	mutate(param=if_else(param=='beta[2]','Light_night',param)) %>% 
-	mutate(param=if_else(param=='gamma','Date',param)) %>% 
-	mutate(param=if_else(param=='alpha','Time (Day_v_Night)',param)) %>% 
-	mutate(param=if_else(param=='theta','Bio_rep',param)) %>% 
-	mutate(param=if_else(param=='intercept','Intercept',param)) %>% 
-	filter(!param=='phi') %>% 
-	mutate(param = factor(param, 
-												levels = c("Intercept", "Date", "Time (Day_v_Night)", "Light_day", "Light_night", "Bio_rep")))
-
-fig_1 <- output %>% 
-	ggplot()+
-	geom_point(aes(x=param,y=mean))+
-	geom_errorbar(aes(x=param,ymin=`2.5%`,ymax=`97.5%`),width=0.2) +
-	theme_bw()+
-	labs(y='Dissimilarity effect (logit-scale)',
-			 x= 'Parameters')+
-	ggtitle(names_of_iterations[k])+
-	theme(axis.text = element_text(size=14),
-				axis.title = element_text(size=16),
-				axis.text.x = element_text(angle = 45, vjust = 1.0, hjust = 1))
-
-ggsave(here('plots',paste0('Fig_1_',names_of_iterations[k],'.jpg')),fig_1,width = 10,height = 8)
-
-fig_2 <- design_mat %>% 
-	group_by(time_cat,tre_cat,delta_date,rep_cat) %>% 
-	mutate(D_mean=mean(D)) %>% 
-	mutate(I=output$mean[output$param=='Intercept']) %>%
-	mutate(gamma=output$mean[output$param=='Date']) %>% 
-	mutate(alpha=output$mean[output$param=='Time (Day_v_Night)']) %>% 
-	mutate(beta_1=output$mean[output$param=='Light_day']) %>% 
-	mutate(beta_2=output$mean[output$param=='Light_night']) %>% 
-	mutate(theta=output$mean[output$param=='Bio_rep']) %>% 
-	mutate(beta=if_else(beta_idx==2,beta_2,beta_1)) %>% 
-	mutate(D_est_logit=
-				 	I+
-				 	gamma*delta_date+
-				 	alpha * time_cat+
-				 	beta * tre_cat+
-				 	theta * rep_cat) %>% 
-	mutate(D_est=inv_logit(D_est_logit)) %>% 
-	ggplot()+
-	geom_point(aes(x=D,y=D_est),alpha=0.3,pch=4)+
-	geom_point(aes(x=D_mean,y=D_est),size=4,color='red')+
-	theme_bw()+
-	ggtitle(names_of_iterations[[k]])+
-	geom_abline(intercept = 0,slope=1,lty=2)+
-	labs(x='Estimated Dissimilarity',
-			 y='Predicted Dissimilarity')
-ggsave(here('plots',paste0('Fig_2_',names_of_iterations[k],'.jpg')),fig_2,width = 10,height = 8)
-
-
-fig_3 <- design_mat %>% 
-	mutate(beta_idx=if_else(tre_cat==0,0,beta_idx)) %>%
-	group_by(time_cat,tre_cat,delta_date,rep_cat,beta_idx) %>%
-	summarise(D_mean=mean(D)) %>% 
-	mutate(I=output$mean[output$param=='Intercept']) %>%
-	mutate(gamma=output$mean[output$param=='Date']) %>% 
-	mutate(alpha=output$mean[output$param=='Time (Day_v_Night)']) %>% 
-	mutate(beta_1=output$mean[output$param=='Light_day']) %>% 
-	mutate(beta_2=output$mean[output$param=='Light_night']) %>% 
-	mutate(theta=output$mean[output$param=='Bio_rep']) %>% 
-	mutate(beta=if_else(beta_idx==2,beta_2,beta_1)) %>% 
-	mutate(D_est_logit=
-				 	I+
-				 	gamma*delta_date+
-				 	alpha * time_cat+
-				 	beta * tre_cat) %>% 
-				 	# theta * rep_cat) %>%
-	mutate(D_est=inv_logit(D_est_logit)) %>% 
-	mutate(group=if_else(time_cat==0,'Same_time_of_day',NA)) %>% 
-	mutate(group=if_else(time_cat==1,'Diff_time_of_day',group)) %>% 
-	mutate(group=if_else(time_cat==0&tre_cat==1&beta_idx==1,'Day_light_effect',group)) %>% 
-	mutate(group=if_else(time_cat==0&tre_cat==1&beta_idx==2,'Night_light_effect',group)) %>% 
-	ggplot()+
-	geom_line(aes(x=delta_date,y=D_est,colour = factor(group)),size=1,lty=2)+
-	geom_point(aes(x=delta_date,y=D_mean,colour = factor(group)),size=3)+
-	ggtitle(names_of_iterations[[k]])+
-	scale_color_manual(
-		values = c("Same_time_of_day" = "#1f78b4",
-							 "Diff_time_of_day" = "#e31a1c",
-							 "Day_light_effect" = "#33a02c",
-							 "Night_light_effect" = "#6a3d9a"),
-		name = "Group",
-		labels = c("Light effect during day", "Different ToD", "Light effect during night", "Same ToD"))+
-	labs(x='Time',
-				 y='Community dissimilarity')+
-	theme_bw()
-ggsave(here('plots',paste0('Fig_3_',names_of_iterations[k],'.jpg')),fig_3,width = 12,height = 8)
+	design_mat <- design_mat_list[[k]]
+	
+	# model <- betareg(D ~ delta_date + time_cat + tre_cat + beta_idx + rep_cat, data = design_mat, link = "logit")
+	model <- betareg(D ~ delta_date + time_cat + light_day + light_night + rep_cat, data = design_mat, link = "logit")
+	
+	s[[k]] <- summary(model)$coefficients$mean %>% as.data.frame() %>% rownames_to_column('param') %>% 
+		mutate(param=if_else(param=='delta_date','Time',param)) %>% 
+		mutate(param=if_else(param=='time_cat','Samp_period',param)) %>% 
+		mutate(param=if_else(param=='light_day','Light_day',param)) %>% 
+		mutate(param=if_else(param=='light_night','Light_night',param)) %>% 
+		mutate(param=if_else(param=='rep_cat','Bio_rep',param)) %>% 
+		mutate(param=if_else(param=='(Intercept)','Intercept',param)) %>% 
+		mutate(param = factor(param,
+													levels = c("Intercept", "Time", "Samp_period", "Light_day", "Light_night", "Bio_rep")))
+	
 }
+
+# Plot fig 1 (make sure you have selected the correct model output s[[1]]=COI on eDNAidx);
+# Check names_of_iterations[k] to know which data has been inputed in the model
+
+fig_1 <-
+		s[[1]] %>%
+		mutate(lo=Estimate-`Std. Error`) %>% 
+		mutate(up=Estimate+`Std. Error`) %>% 
+		ggplot()+
+		geom_point(aes(x=param,y=Estimate))+
+		geom_errorbar(aes(x=param,ymin=lo,ymax=up),width=0.2) +
+		theme_bw()+
+		labs(y='Dissimilarity effect (logit-scale)',
+				 x= 'Parameters')+
+		theme(axis.text = element_text(size=14),
+					axis.title = element_text(size=16),
+					axis.text.x = element_text(angle = 45, vjust = 1.0, hjust = 1))
+	
+	# ggsave(here('plots',paste0('Fig_1_',names_of_iterations[k],'.jpg')),fig_1,width = 10,height = 8)
+	
+# Creating the data that will be used for figure 3
+# First creating the COI eDNA idx
+output <- s[[1]]
+plot_dat1 <- design_mat_list[[1]] %>%
+		group_by(time_cat,tre_cat,delta_date,rep_cat,light_day,light_night) %>%
+		summarise(D_mean=mean(D)) %>%
+		mutate(I=output$Estimate[output$param=='Intercept']) %>%
+		mutate(gamma=output$Estimate[output$param=='Time']) %>%
+		mutate(alpha=output$Estimate[output$param=='Samp_period']) %>%
+		mutate(beta_1=output$Estimate[output$param=='Light_day']) %>%
+		mutate(beta_2=output$Estimate[output$param=='Light_night']) %>%
+		mutate(theta=output$Estimate[output$param=='Bio_rep']) %>%
+		mutate(beta=if_else(light_day==1,beta_1,beta_2)) %>%
+		mutate(beta=if_else(tre_cat==0,0,beta)) %>%
+		mutate(D_est_logit=
+					 	I+
+					 	gamma*delta_date+
+					 	alpha * time_cat+
+					 	beta * tre_cat) %>%
+		# theta * rep_cat) %>%
+		mutate(D_est=inv_logit(D_est_logit)) %>%
+		mutate(group=if_else(time_cat==0,'Same_time_of_day',NA)) %>%
+		mutate(group=if_else(time_cat==1,'Diff_time_of_day',group)) %>%
+		mutate(group=if_else(time_cat==0&tre_cat==1&light_day==1,'Day_light_effect',group)) %>%
+		mutate(group=if_else(time_cat==0&tre_cat==1&light_night==1,'Night_light_effect',group)) %>% 
+	ungroup() %>% 
+	select(delta_date,D_est,D_mean,group)
+
+# Then creating the MV1 eDNA idx
+output <- s[[4]]
+plot_dat2 <- design_mat_list[[4]] %>%
+		group_by(time_cat,tre_cat,delta_date,rep_cat,light_day,light_night) %>%
+		summarise(D_mean=mean(D)) %>%
+		mutate(I=output$Estimate[output$param=='Intercept']) %>%
+		mutate(gamma=output$Estimate[output$param=='Time']) %>%
+		mutate(alpha=output$Estimate[output$param=='Samp_period']) %>%
+		mutate(beta_1=output$Estimate[output$param=='Light_day']) %>%
+		mutate(beta_2=output$Estimate[output$param=='Light_night']) %>%
+		mutate(theta=output$Estimate[output$param=='Bio_rep']) %>%
+		mutate(beta=if_else(light_day==1,beta_1,beta_2)) %>%
+		mutate(beta=if_else(tre_cat==0,0,beta)) %>%
+		mutate(D_est_logit=
+					 	I+
+					 	gamma*delta_date+
+					 	alpha * time_cat+
+					 	beta * tre_cat) %>%
+		# theta * rep_cat) %>%
+		mutate(D_est=inv_logit(D_est_logit)) %>%
+		mutate(group=if_else(time_cat==0,'Same_time_of_day',NA)) %>%
+		mutate(group=if_else(time_cat==1,'Diff_time_of_day',group)) %>%
+		mutate(group=if_else(time_cat==0&tre_cat==1&light_day==1,'Day_light_effect',group)) %>%
+		mutate(group=if_else(time_cat==0&tre_cat==1&light_night==1,'Night_light_effect',group)) %>% 
+	ungroup() %>% 
+	select(delta_date,D_est,D_mean,group)
+
+# Then joiing the two data together to be able to plot them together
+fig_3 <- 
+	plot_dat1 %>% mutate(marker='COI') %>% rbind(.,plot_dat2 %>% mutate(marker='MV1')) %>% 
+		ggplot()+
+		geom_line(aes(x=delta_date,y=D_est,colour = factor(group)),size=1,lty=2)+
+		geom_point(aes(x=delta_date,y=D_mean,colour = factor(group)),size=3)+
+		# ggtitle(names_of_iterations[[k]])+
+		scale_color_manual(
+			values = c("Same_time_of_day" = "#1f78b4",
+								 "Diff_time_of_day" = "#e31a1c",
+								 "Day_light_effect" = "#33a02c",
+								 "Night_light_effect" = "#6a3d9a"),
+			name = "Group",
+			labels = c("Light effect during day", "Different time of day", "Light effect during night", "Same time of day"))+
+		labs(x='Time (days)',
+				 y='Community dissimilarity')+
+	facet_wrap(~marker)+
+		theme_bw()+
+	theme(axis.title = element_text(size=15),
+				axis.text = element_text(size=14),
+				legend.text = element_text(size=14),
+				legend.title = element_blank(),
+				legend.position = 'bottom')
+	ggsave(here('plots',paste0('Fig_3_xxx','.jpg')),fig_3,width = 13,height = 8)
+
 # c("#1f78b4", "#e31a1c", "#33a02c", "#6a3d9a")
